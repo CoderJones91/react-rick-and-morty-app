3191073ba6f9d993808b9b2c9497aec64706f008
CoderJones91 <acjones0391@gmail.com>
2025-03-14 01:00:36 -0600

    update nav

--- my-rick-and-morty-app/src/components/NavBar/navbar.js
+++ my-rick-and-morty-app/src/components/NavBar/navbar.js
@@ -1,15 +1,16 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark-opacity">
       <div className="container-fluid">
-        <a className="text-decoration-none navbar-brand" href="/">
+        <NavLink className="text-decoration-none navbar-brand" to="/">
           <h1 className="fs-2 m-1">
             <span role="img" aria-label="planet">&#128760;</span> Rick and Morty Wiki
             <span role="img" aria-label="rocket"> &#128640;</span>
           </h1>
-        </a>
+        </NavLink>
         <button
           className="navbar-toggler"
           type="button"
@@ -22,28 +23,27 @@ const NavBar = () => {
           <style jsx>
             {`
               button[aria-expanded="false"] > .close {
-              display: none;
+                display: none;
               }
               button[aria-expanded="true"] > .open {
-              display: none;
+                display: none;
               }
             `}
           </style>
           
           <i className="fa-solid fa-bars-staggered open text-light"></i>
           <i className="fa-solid fa-x close fw-bold text-light"></i>
-         
         </button>
         <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
           <ul className="navbar-nav">
             <li className="nav-item">
-              <a className="nav-link" href="/">Characters</a>
+              <NavLink className="nav-link" to="/">Characters</NavLink>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="/episodes">Episodes</a>
+              <NavLink className="nav-link" to="/episodes">Episodes</NavLink>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="/location">Locations</a>
+              <NavLink className="nav-link" to="/location">Locations</NavLink>
             </li>
           </ul>
         </div>
