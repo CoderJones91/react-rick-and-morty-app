2a9105a9ec32275b21fa2534f82784f2c083c502
CoderJones91 <acjones0391@gmail.com>
2025-03-14 00:41:03 -0600

    update filter

--- my-rick-and-morty-app/src/components/Filter/FilterBTN.js
+++ my-rick-and-morty-app/src/components/Filter/FilterBTN.js
@@ -1,18 +1,27 @@
 import React from "react";
 
-const FilterBTN = ({name, index, items}) => {
+const FilterBTN = ({name, index, items, task, setPageNumber }) => {
     return (
       
         <div>
           <style jsx>
         {`
+        .x:checked + label{
+            background-color: #6db474;
+            color: #fff;
+        }
         input[type="radio"]{
         display: none;
         }
         `}
         </style>
         <div className="form-check">
-  <input className="form-check-input" type="radio" name={name} id={`${name}-${index}`} />
+  <input 
+  onClick={() => {
+    setPageNumber(1);
+    task(items);
+  }}
+  className="form-check-input x" type="radio" name={name} id={`${name}-${index}`} />
   <label class="btn btn-outline-primary" for={`${name}-${index}`}>{items}</label>
 </div>
 </div>
