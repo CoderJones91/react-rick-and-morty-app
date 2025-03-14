4ae8e1124da688c1dc2c4e56aa469821a1b67d9a
CoderJones91 <acjones0391@gmail.com>
2025-03-14 01:06:48 -0600

    update search

--- my-rick-and-morty-app/src/components/Search/search.js
+++ my-rick-and-morty-app/src/components/Search/search.js
@@ -1,16 +1,10 @@
 import React from "react";
 import styles from "./search.module.scss";
 
-const searchInput = document.getElementById("searchInput");
-const clearButton = document.getElementById("clearButton");
-
 const Search = ({ setSearch, setPageNumber }) => {
-  let searchBtn = (e) => {
-    e.preventDefault();
-  };
   return (
     <form
-      className={`${styles.search} d-flex flex-sm-row flex-column align-items-center justify-content-center gap-4 mb-5`}
+      className={`${styles.search} d-flex flex-sm-row flex-column align-items-center justify-content-center gap-4 mb-5 mt-5`}
     >
       <input type="search" onChange={(e) => {
           setPageNumber(1);
